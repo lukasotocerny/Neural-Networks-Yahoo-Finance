b64abe7a262992edce2c8f1131f99817fb0d873c
Lukas Cerny <cerny.lukas.oto@gmail.com>
2016-12-03 23:08:58 +0000

    user interaction

--- SingleLayerPerceptron.hs
+++ SingleLayerPerceptron.hs
@@ -1,4 +1,5 @@
 -- Single Layer Perceptrons
+import Data.List
 import System.Random
 
 type Weight = Double
@@ -33,15 +34,45 @@ success neuron (x:xs) = evaluate neuron (fst x) == (snd x) && success neuron xs
 
 -- Iterates the training n times. If successful, it breaks.
 iterate_train :: Perceptron -> [([Double],Int)] -> Int -> Perceptron
-iterate_train neuron xs 0 = error "Cannot train Perceptron. Data are hard to clasify."
+iterate_train neuron xs 0 = neuron
 iterate_train neuron xs i
-    | success neuron xs = neuron
     | otherwise = iterate_train (train neuron xs) xs (i - 1)
 
 buildNeuron :: Int -> Perceptron
-buildNeuron x = Neuron [ randomRIO (0.0,1.0) | i <- [1..x] ] (round randomRIO (0.0,1.0) getStdGen) 
+buildNeuron x = Neuron [ 1.0 | i <- [1..x] ] (1.0) 
+
+getTLines :: Int -> IO [String]
+getTLines 0 = return []
+getTLines t = do
+    x <- getLine
+    xs <- getTLines (t-1)
+    return (x:xs)
+
+createData :: [String] -> [([Double],Int)]
+createData xs = [ (init test, round (last test)) | test <- doubles ]
+    where
+    doubles = [ map read (words x) | x <- xs ] :: [[Double]]
 
 main = do 
     putStrLn ("Write the number of input neurons:")
-    nInput <- readLn :: IO Int
-    putStrLn (show nInput)
+    n_temp <- getLine
+    let n = read n_temp :: Int
+    putStrLn ("Write the number of test cases:")
+    t_temp <- getLine
+    let t = read t_temp :: Int
+    putStrLn ("Write the test cases in form: i1 i2 .. in t")
+    putStrLn ("Where t is target output, i are inputs.")
+    putStrLn ("For example: 1 4 1 1 0")
+    d_temp <- getTLines t
+    putStrLn ("How many times do you want to iterate training?")
+    i_temp <- getLine
+    let i = read i_temp :: Int
+    let dataset = createData d_temp
+    let neuron = buildNeuron n
+    let neuron' = iterate_train neuron dataset i
+    if success neuron' dataset then do
+        putStrLn ("Successfully trained. Resulting weights are:")
+        putStrLn (show neuron')
+    else do
+        putStrLn ("Neuron not trained on the dataset. Weights after training are:")
+        putStrLn (show neuron')
